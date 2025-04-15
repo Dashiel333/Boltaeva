553fa87aa7cdd53284f717f5f6ff9ea2781fadaf
Daria <daria.vildanova.07@gmail.com>
2025-04-15 13:10:33 +0300

    JS code

--- js/main.js
+++ js/main.js
@@ -13,3 +13,23 @@ document.querySelector(".bourger").addEventListener("click", function () {
 document.getElementById("close").addEventListener("click", function () {
     document.getElementById("menu").classList.remove("active");
 });
+
+const searchContainer = document.querySelector(".search-container");
+const openBtn = document.querySelector(".icon-logo");
+const closeMenu = document.querySelector(".dark-color");
+const closeBtn = document.querySelector(".search-container .close")
+
+openBtn.addEventListener("click", function () {
+    searchContainer.classList.toggle('active');
+});
+console.log(searchContainer, openBtn); 
+
+closeMenu.addEventListener("click", function(){
+    searchContainer.classList.remove('active');
+    closeMenu.classList.remove('active');
+})
+closeBtn.addEventListener("click", function(){
+    searchContainer.classList.remove('active');
+    closeMenu.classList.remove('active');
+})
+console.log(closeBtn);
